c94448cd51ecff5f0bca8a2dbe01f3b9805a1247
RodrigoUma04 <rodrigo552147@outlook.be>
2026-06-08 00:46:33 +0200

    Add auth service spotless run to the pre commit config

--- backend/auth/src/test/java/com/fennixs/auth/TestAuthApplication.java
+++ backend/auth/src/test/java/com/fennixs/auth/TestAuthApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 public class TestAuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(AuthApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(AuthApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
     }
-
 }
