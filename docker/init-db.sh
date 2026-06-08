7de2cc2e15e4f0089dd3ab6fe4de6cb074debd6e
RodrigoUma04 <rodrigo552147@outlook.be>
2026-06-08 16:36:29 +0200

    Address PR review findings in infrastructure and CI

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -2,6 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE fennixs_auth;
-    CREATE DATABASE fennixs_core;
+    SELECT 'CREATE DATABASE fennixs_auth'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fennixs_auth')\gexec
+
+    SELECT 'CREATE DATABASE fennixs_core'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fennixs_core')\gexec
 EOSQL
